edf978fd0d96ec81203dad7a9b062fc0b0cbaf32
Alexandr Yanykin <numelen@gmail.com>
2022-02-20 14:12:02 +0300

    chore: icons folder structure changed

---
+++ react-hooks/src/assets/illustrations/index.ts
@@ -0,0 +1,10 @@
+import failure from './icons/failure.svg';
+import github from './icons/github.svg';
+import repeat from './icons/repeat.svg';
+import reverse from './icons/reverse.svg';
+import star from './icons/star.svg';
+import starWin from './icons/starWin.svg';
+import success from './icons/success.svg';
+import telegram from './icons/telegram.svg';
+
+export { failure, github, repeat, reverse, star, starWin, success, telegram };
