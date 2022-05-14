33fd3651e4d03f6900881254ecf88675d22de129
Alexandr Yanykin <numelen@gmail.com>
2022-05-14 11:42:27 +0300

    feat: migrating formingListWords to ts

--- src/pages/Statistics/utils/formingListWords.ts
+++ src/pages/Statistics/utils/formingListWords.ts
@@ -1,9 +1,16 @@
+import { StatisticsItem } from 'types/types';
+
 export const formingListWords = () => {
-  const data = JSON.parse(localStorage.getItem('data'));
+  const data: StatisticsItem[] = JSON.parse(
+    localStorage.getItem('data') || '[]',
+  );
+
   data.sort((a, b) => (a.wrong > b.wrong ? -1 : 1));
+
   const array = [];
   for (let i = 0; i < 8; i += 1) {
     if (data[i].wrong > 0) array.push(data[i]);
   }
+
   localStorage.setItem('difficult-words', JSON.stringify(array));
 };
