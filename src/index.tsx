982821be50cb8cb9c8ac07e03066636f7d90f210
Alexandr Yanykin <numelen@gmail.com>
2022-05-21 15:29:25 +0300

    feat: added redux to app

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,11 @@
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { App } from 'App';
+import { store } from 'redux-react/store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
