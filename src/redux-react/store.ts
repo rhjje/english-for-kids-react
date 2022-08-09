8e484bf7fdb9a3f038e648a3b9b1885b4cc3c0f2
Alexandr Yanykin <numelen@gmail.com>
2022-08-09 23:07:33 +0300

    feat: added reduxjs/toolkit

--- src/redux-react/store.ts
+++ src/redux-react/store.ts
@@ -1,6 +1,6 @@
-import { legacy_createStore as createStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import { reducer } from './reducer';
 
-export const store = createStore(reducer);
+export const store = configureStore({ reducer });
 
 export type AppState = ReturnType<typeof store.getState>;
